ded12f2c05d228711248d7d5005c904c1b8b77a4
Jerrynicki <38357030+Jerrynicki@users.noreply.github.com>
2019-01-30 20:39:53 +0100

    C#

---
+++ C#/doener.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace doener
+{
+    public static class Doener
+    {
+        public static void mit(string senf)
+        {
+            Console.WriteLine("döner mit senf");
+        }
+    }
+}
+
