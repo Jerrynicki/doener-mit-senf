cb3943f28adcaafd367de3a674bf4a3f888c8431
M3IY0U <timo.nock@gmx.de>
2019-01-30 21:48:36 +0100

    top 10 ehrenloseste aktionen

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,11 +1,5 @@
 #include "Doener.h"
 #include <iostream>
 int main() {
-	setlocale(LC_ALL, "");
-	std::string input;
-	std::cout << "senf?" << std::endl;
-	std::getline(std::cin, input);
-	std::transform(input.begin(), input.end(), input.begin(), ::tolower);
-	std::cout << Doener::mit(input);
-	std::cin.ignore();
+	std::cout << Doener::mit("senf");
 }
